1da0cf34b9ee166241b15e8be8126a563437d351
Chris Turner <peavers@gmail.com>
2025-04-30 15:02:27 -0500

    feat: Standardized addon announcements

--- src/Main.lua
+++ src/Main.lua
@@ -11,106 +11,105 @@ PAS.version = C_AddOns.GetAddOnMetadata(addonName, "Version") or "1.0.0"
 
 -- Icon names for reference
 local iconNames = {
-    [1] = "Star", [2] = "Circle", [3] = "Diamond", [4] = "Triangle",
-    [5] = "Moon", [6] = "Square", [7] = "Cross", [8] = "Skull"
+	[1] = "Star", [2] = "Circle", [3] = "Diamond", [4] = "Triangle",
+	[5] = "Moon", [6] = "Square", [7] = "Cross", [8] = "Skull"
 }
 
 -- Function to find and mark the tank
 local function MarkTank()
-    if not IsInGroup() then
-        Utils.Debug(PAS, "Not in group")
-        return
-    end
+	if not IsInGroup() then
+		Utils.Debug(PAS, "Not in group")
+		return
+	end
 
-    if IsInRaid() then
-        Utils.Debug(PAS, "In raid - disabled")
-        return
-    end
+	if IsInRaid() then
+		Utils.Debug(PAS, "In raid - disabled")
+		return
+	end
 
-    local numMembers = GetNumGroupMembers()
-    for i = 1, numMembers do
-        local unit = i < numMembers and "party" .. i or "player"
-        if UnitGroupRolesAssigned(unit) == "TANK" then
-            local name = UnitName(unit)
-            local currentMark = GetRaidTargetIndex(unit)
-            if currentMark ~= PAS.Config.iconId then
-                SetRaidTarget(unit, PAS.Config.iconId)
-                Utils.Debug(PAS, "Marked " .. name .. " with " .. iconNames[PAS.Config.iconId])
-            end
-            return
-        end
-    end
+	local numMembers = GetNumGroupMembers()
+	for i = 1, numMembers do
+		local unit = i < numMembers and "party" .. i or "player"
+		if UnitGroupRolesAssigned(unit) == "TANK" then
+			local name = UnitName(unit)
+			local currentMark = GetRaidTargetIndex(unit)
+			if currentMark ~= PAS.Config.iconId then
+				SetRaidTarget(unit, PAS.Config.iconId)
+				Utils.Debug(PAS, "Marked " .. name .. " with " .. iconNames[PAS.Config.iconId])
+			end
+			return
+		end
+	end
 
-    Utils.Debug(PAS, "No tank found")
+	Utils.Debug(PAS, "No tank found")
 end
 
 -- Register slash commands
-PeaversCommons.SlashCommands:Register(addonName, "tm", {
-    default = function()
-        Utils.Print(PAS, "Manual check performed")
-        MarkTank()
-    end,
-    debug = function()
-        PAS.Config.debugMode = not PAS.Config.debugMode
-        PAS.Config.DEBUG_ENABLED = PAS.Config.debugMode
-        PAS.Config:Save()
-        Utils.Print(PAS, "Debug mode " .. (PAS.Config.debugMode and "enabled" or "disabled"))
-    end,
-    icon = function(rest)
-        local iconId = tonumber(rest)
-        if iconId and iconId >= 1 and iconId <= 8 then
-            PAS.Config.iconId = iconId
-            PAS.Config:Save()
-            Utils.Print(PAS, "Using icon: " .. iconNames[iconId])
-        else
-            Utils.Print(PAS, "Invalid icon (use 1-8)")
-        end
-    end,
-    help = function()
-        Utils.Print(PAS, "Commands:")
-        print("  /tm - Manual check")
-        print("  /tm icon N - Set icon (1-8)")
-        print("  /tm debug - Toggle debug mode")
-        print("  /tm config - Open settings")
-    end
+PeaversCommons.SlashCommands:Register(addonName, "pas", {
+	default = function()
+		Utils.Print(PAS, "Manual check performed")
+		MarkTank()
+	end,
+	debug = function()
+		PAS.Config.debugMode = not PAS.Config.debugMode
+		PAS.Config.DEBUG_ENABLED = PAS.Config.debugMode
+		PAS.Config:Save()
+		Utils.Print(PAS, "Debug mode " .. (PAS.Config.debugMode and "enabled" or "disabled"))
+	end,
+	icon = function(rest)
+		local iconId = tonumber(rest)
+		if iconId and iconId >= 1 and iconId <= 8 then
+			PAS.Config.iconId = iconId
+			PAS.Config:Save()
+			Utils.Print(PAS, "Using icon: " .. iconNames[iconId])
+		else
+			Utils.Print(PAS, "Invalid icon (use 1-8)")
+		end
+	end,
+	help = function()
+		Utils.Print(PAS, "Commands:")
+		print("  /pas - Manual check")
+		print("  /pas icon N - Set icon (1-8)")
+		print("  /pas debug - Toggle debug mode")
+		print("  /pas config - Open settings")
+	end
 })
 
 -- Initialize the addon
 PeaversCommons.Events:Init(addonName, function()
-    -- Initialize configuration
-    PAS.Config:Initialize()
-    
-    -- Initialize configuration UI
-    if PAS.ConfigUI and PAS.ConfigUI.Initialize then
-        PAS.ConfigUI:Initialize()
-    end
-    
-    -- Initialize support UI
-    if PAS.SupportUI and PAS.SupportUI.Initialize then
-        PAS.SupportUI:Initialize()
-    end
-    
-    -- Register events
-    PeaversCommons.Events:RegisterEvent("GROUP_ROSTER_UPDATE", function()
-        MarkTank()
-    end)
-    
-    PeaversCommons.Events:RegisterEvent("READY_CHECK", function()
-        MarkTank()
-    end)
-    
-    PeaversCommons.Events:RegisterEvent("PLAYER_ENTERING_WORLD", function()
-        MarkTank()
-    end)
-    
-    -- Set up periodic checking
-    PeaversCommons.Events:RegisterOnUpdate(PAS.Config.checkFrequency, function()
-        if PAS.Config.enabled then
-            MarkTank()
-        end
-    end, "PAS_TankMarker")
-    
-    -- Initialization message
-    Utils.Print(PAS, "Loaded! Disabled in raids.")
-    Utils.Print(PAS, "Type /tm help for commands")
-end)
+	-- Initialize configuration
+	PAS.Config:Initialize()
+
+	-- Initialize configuration UI
+	if PAS.ConfigUI and PAS.ConfigUI.Initialize then
+		PAS.ConfigUI:Initialize()
+	end
+
+	-- Initialize support UI
+	if PAS.SupportUI and PAS.SupportUI.Initialize then
+		PAS.SupportUI:Initialize()
+	end
+
+	-- Register events
+	PeaversCommons.Events:RegisterEvent("GROUP_ROSTER_UPDATE", function()
+		MarkTank()
+	end)
+
+	PeaversCommons.Events:RegisterEvent("READY_CHECK", function()
+		MarkTank()
+	end)
+
+	PeaversCommons.Events:RegisterEvent("PLAYER_ENTERING_WORLD", function()
+		MarkTank()
+	end)
+
+	-- Set up periodic checking
+	PeaversCommons.Events:RegisterOnUpdate(PAS.Config.checkFrequency, function()
+		if PAS.Config.enabled then
+			MarkTank()
+		end
+	end, "PAS_TankMarker")
+end, {
+	-- Standardized announcement message
+	announceMessage = "Type /pas help for options."
+})
